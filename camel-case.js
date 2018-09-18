4297217c2001ae1f670e0c28eb4852dd1dc1a576
gucong <gucong@gmail.com>
2018-09-18 12:05:25 +0800

    fix camelCase

--- camel-case.js
+++ camel-case.js
@@ -1,8 +1,11 @@
 "use strict";
 function camelCase (str) {
 	return str.replace(/[\w-]+/g, (s) => (
-		/^-?([a-z]+(?:-[a-z]+)+)$/.test(s)
-			? RegExp.$1.replace(
+		/^-?[a-z]+(?:-[a-z]+)+$/.test(s)
+			? s.replace(
+				/^-(ms|moz|khtml|epub|(\w+-?)*webkit)(?=-)/i,
+				"$1"
+			).replace(
 				/-\w/g,
 				s => (
 					s[1].toUpperCase()
--- test/non-style.js
+++ test/non-style.js
@@ -1,7 +1,7 @@
 "use strict";
 const spawnSync = require("child_process").spawnSync;
 const fs = require("fs");
-const files = spawnSync("git", ["ls-files"], {encoding: "utf8"}).stdout.match(/^.+$/gm).filter(file => file.endsWith(".js"));
+const files = spawnSync("git", ["ls-files"], { encoding: "utf8" }).stdout.match(/^.+$/gm).filter(file => file.endsWith(".js"));
 const syntax = require("../");
 const expect = require("chai").expect;
 

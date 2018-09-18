06f7906adb83fa8d17345e189d7944599730b4c5
ota <otameshiyo23@gmail.com>
2018-09-18 10:53:32 +0900

    Fixed not working with @babel/core@7.0.0. (#33)
    * Fixed not working with @babel/core@7.0.0.
    
    * Like "baleb-eslint" set "decoratorsBeforeExport": false.

--- extract.js
+++ extract.js
@@ -55,7 +55,7 @@ const plugins = [
 	"jsx",
 	"typescript",
 	"objectRestSpread",
-	"decorators",
+	["decorators", { "decoratorsBeforeExport": false }],
 	"classProperties",
 	"exportExtensions",
 	"asyncGenerators",
